14046c413ffc88a14e718d435c91176c2d51a7c9
100の人 <100@pokemori.jp>
2022-10-02 11:59:44 +0900

    テンプレートのテクスチャの圧縮設定を、入稿ルールに合わせて修正 (ASTC compressed 6x6 に)

--- Assets/Editor/Utility.cs
+++ Assets/Editor/Utility.cs
@@ -5,31 +5,57 @@ using UnityEditor;
 public static class Utility
 {
 
-    /*[InitializeOnLoadMethod]
+    //[InitializeOnLoadMethod]
     private static void Run()
     {
-        var pattern = new Regex("^Assets/VitDeck/Templates/JQuesta_Event_Booth_[^/]+/TemplateAssets/{ID}/Interior_[^/]+/Interior_[^/]+.prefab");
-        foreach (var path in AssetDatabase.GetAllAssetPaths())
-        {
-            if (!pattern.IsMatch(path))
-            {
-                continue;
-            }
+        //var pattern = new Regex("^Assets/VitDeck/Templates/JQuesta_.+");
+        //foreach (var path in AssetDatabase.GetAllAssetPaths())
+        //{
+        //    if (!pattern.IsMatch(path))
+        //    {
+        //        continue;
+        //    }
 
-            Debug.Log(path);
+        //    var importer = AssetImporter.GetAtPath(path) as TextureImporter;
+        //    if (importer == null)
+        //    {
+        //        continue;
+        //    }
 
-            var prefab = PrefabUtility.LoadPrefabContents(path);
+        //    Debug.Log(path);
 
-            foreach (var transform in prefab.GetComponentsInChildren<Transform>())
-            {
-                GameObjectUtility.SetStaticEditorFlags(
-                    transform.gameObject,
-                    StaticEditorFlags.OccludeeStatic | StaticEditorFlags.BatchingStatic | StaticEditorFlags.ReflectionProbeStatic
-                );
-            }
+        //    var settings = importer.GetPlatformTextureSettings("Android");
+        //    settings.overridden = true;
+        //    settings.format = TextureImporterFormat.ASTC_6x6;
+        //    importer.SetPlatformTextureSettings(settings);
+        //    EditorUtility.SetDirty(importer);
+        //    importer.SaveAndReimport();
+        //}
 
-            PrefabUtility.SaveAsPrefabAsset(prefab, path);
-            PrefabUtility.UnloadPrefabContents(prefab);
-        }
-    }*/
+
+
+        //var pattern = new Regex("^Assets/VitDeck/Templates/JQuesta_Event_Booth_[^/]+/TemplateAssets/{ID}/Interior_[^/]+/Interior_[^/]+.prefab");
+        //foreach (var path in AssetDatabase.GetAllAssetPaths())
+        //{
+        //    if (!pattern.IsMatch(path))
+        //    {
+        //        continue;
+        //    }
+
+        //    Debug.Log(path);
+
+        //    var prefab = PrefabUtility.LoadPrefabContents(path);
+
+        //    foreach (var transform in prefab.GetComponentsInChildren<Transform>())
+        //    {
+        //        GameObjectUtility.SetStaticEditorFlags(
+        //            transform.gameObject,
+        //            StaticEditorFlags.OccludeeStatic | StaticEditorFlags.BatchingStatic | StaticEditorFlags.ReflectionProbeStatic
+        //        );
+        //    }
+
+        //    PrefabUtility.SaveAsPrefabAsset(prefab, path);
+        //    PrefabUtility.UnloadPrefabContents(prefab);
+        //}
+    }
 }
